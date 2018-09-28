3fdf38b09cfd10572ca96238e9c4ba7f318282be
NightShadow3000 <RoyalFotoK@gmail.com>
2018-09-27 20:28:48 -0400

    Practice Code

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+
+
+
+//        Scanner input = new Scanner(System.in);
+//
+//        String alice = "alicE";
+//        String bob = "bOB";
+//        String guess = "";
+//        int numTries = 0;
+//        boolean win = false;
+//
+//        while(!win) {
+//            if (!guess.equals(alice) || !guess.equals(bob)) {
+//                System.out.println("What's your name?");
+//                guess = input.nextLine();
+//                numTries++;
+//            }
+//
+//            if (guess.equalsIgnoreCase(alice) || guess.equalsIgnoreCase(bob)) {
+//                win = true;
+//                System.out.println("Welcome " + guess);
+//                System.out.println("You made " + (numTries - 1) + " wrong tries!");
+//            }
+//        }
+
+
+    }// end of psvm
+}
